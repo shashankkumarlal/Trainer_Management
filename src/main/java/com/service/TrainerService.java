91805a2ee5c901afffdf5c758f81a38ff5beb5d5
AhanaS07 <ahanasrinathmurthy@gmail.com>
2026-06-18 13:05:36 +0530

    feat: Add TrainerService class with method stubs

---
+++ src/main/java/com/service/TrainerService.java
@@ -0,0 +1,27 @@
+package com.service;
+
+import com.dao.TrainerDAO;
+import com.util.HibernateUtil;
+import com.entities.Trainer;
+
+public class TrainerService {
+    private final TrainerDAO trainerDAO = new TrainerDAO();
+
+    public Trainer registerTrainer(Trainer t){
+
+    }
+
+    public void updateTrainer(Trainer t){
+
+    }
+
+    public void deleteTrainer(){}
+
+    public Trainer searchTrainer(int id){
+        
+    }
+
+    public List<Trainer> displayAll(){
+
+    }
+}
