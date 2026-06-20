9f5b80d9c2b7e849a8ed1d2d0175bd5852e0769f
Shashank Kumar Lal <shashanklal309@gmail.com>
2026-06-20 22:14:41 +0530

    refactor: add validations in TrainerService

--- src/main/java/com/service/TrainerService.java
+++ src/main/java/com/service/TrainerService.java
@@ -1,27 +1,31 @@
 package com.service;
 
 import com.dao.TrainerDAO;
-import com.util.HibernateUtil;
 import com.entities.Trainer;
+
+import java.util.List;
 
 public class TrainerService {
     private final TrainerDAO trainerDAO = new TrainerDAO();
 
     public Trainer registerTrainer(Trainer t){
-
+        trainerDAO.addTrainer(t);
+        return t;
     }
 
     public void updateTrainer(Trainer t){
-
+        trainerDAO.updateTrainer(t);
     }
 
-    public void deleteTrainer(){}
+    public void deleteTrainer(Long id){
+        trainerDAO.deleteTrainer(id);
+    }
 
-    public Trainer searchTrainer(int id){
-        
+    public Trainer searchTrainer(Long id){
+        return trainerDAO.getTrainerById(id);
     }
 
     public List<Trainer> displayAll(){
-
+        return trainerDAO.showAllTrainer();
     }
 }
